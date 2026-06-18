f35d57f2f633291c482e0728308a9c230801a71a
bhargavi <bhargavi@scan0.io>
2026-06-18 16:33:08 +0530

    Create main.tf

---
+++ src/main/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "test" {
+  bucket = "scan0-discovery-test-bucket"
+}
